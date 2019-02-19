8bc36b32b5ed8c6eca0184306aaa47cdbf8c1392
TheSuperiorStanislav <stasontos2@hotmail.com>
2019-02-19 14:32:21 +0700

    fix static files config

--- message_board/settings.py
+++ message_board/settings.py
@@ -112,6 +112,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManinfestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = 'blog'
 LOGOUT_REDIRECT_URL = 'blog'
